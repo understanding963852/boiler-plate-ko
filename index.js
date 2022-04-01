e93d65a57f133a29fa09910d98ac5e572fa4e244
understanding963952 <suuneeee@naver.com>
2022-04-01 10:59:56 +0900

    ㄹㄹ

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
 })
 
+
+app.post('/register',(req,res)=>{
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
